384563a508b779957232ef80eedb67e772448883
Alex Pattullo <alexpattullo@icloud.com>
2025-05-14 13:01:14 +0100

    fixing config file

--- shared_resources.py
+++ shared_resources.py
@@ -2,7 +2,7 @@ import json
 Active_Rcon_Connections = {}
 
 #Fetch config file
-with open("./testconfig.json", "r") as file:
+with open("./config.json", "r") as file:
     GEN_CONFIG = json.load(file)
 
 
